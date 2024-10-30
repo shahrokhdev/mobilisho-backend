151f5bd08aa55db585b5a54638b87d9d5cab7a73
shahrokhdev <shahrokhghorbanigh8085@gmail.com>
2024-10-30 21:42:28 +0330

    added last articles api

---
+++ app/GraphQL/Queries/LastArticles.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Article;
+
+final  class LastArticles
+{
+    /** @param  array{}  $args */
+    public function __invoke( $_, array $args)
+    {
+        return Article::query()->orderBy('created_at' , "DESC")->take(5)->get();
+    }
+}
