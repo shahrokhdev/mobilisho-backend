9ad2ea8041ea1655b806e118dfba67b10165fbdb
shahrokhdev <shahrokhghorbanigh8085@gmail.com>
2024-11-13 17:35:15 +0330

    added search api by categories and products

---
+++ app/GraphQL/Queries/Search.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Category;
+use App\Models\Product;
+
+final class Search
+{
+    /** @param  array{}  $args */
+
+    public function __invoke($_, array $args)
+    {
+        $keyword = $args['keyword'];
+
+        $productResults = Product::where('title', 'like', "%{$keyword}%")
+        ->orWhere('description', 'like', "%{$keyword}%")
+        ->get();
+    
+    $categoryResults = Category::where('name', 'like', "%{$keyword}%")
+        ->orWhere('slug', 'like', "%{$keyword}%")
+        ->get();
+    
+    $results = $productResults->merge($categoryResults)->sortByDesc('name'); ;
+    
+    return $results->map(function ($result) {
+        if ($result instanceof Product) {
+            return [
+                'id' => $result->id,
+                'title' => $result->title,
+                'description' => $result->description,
+                'model_name' => "Product"
+            ];
+        } elseif ($result instanceof Category) {
+            return [
+                'id' => $result->id,
+                'name' => $result->name,
+                'model_name' => "Category"
+            ];
+        }
+    });
+    }
+
+
+}
